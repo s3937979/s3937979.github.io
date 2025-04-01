2cb06e357103e560fe9b78372ed736ad41d4adea
s3937979 <s3937979@rmit.edu.vn>
2025-04-01 23:48:05 +1100

    update all

--- im/week5/cart/script.js
+++ im/week5/cart/script.js
@@ -3,5 +3,17 @@ let shoppingCart = [
   { name: "Jeans", price: 50 },
   { name: "Sneakers", price: 80 },
   { name: "Backpack", price: 30 },
+  { name: "shades", price: 100 },
 ];
 console.log(shoppingCart);
+let total = 0;
+for (let i = 0; i < shoppingCart.length; i++) {
+  total = total + shoppingCart[i].price;
+  console.log("total so far is", total);
+}
+console.log("Final total is", total);
+let discount = 0.1;
+if (total > 100) {
+  let discountedPrice = total - total * discount;
+  console.log("Discounted price is", discountedPrice);
+}
