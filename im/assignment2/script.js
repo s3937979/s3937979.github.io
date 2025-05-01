26fa906b72b53be0bbeed57381a21771c1e608aa
s3937979 <s3937979@rmit.edu.vn>
2025-05-01 13:22:18 +1000

    update all

--- im/assignment2/script.js
+++ im/assignment2/script.js
@@ -1,9 +1,19 @@
-const lp = document.querySelector(".lp");
-const tonearm = document.querySelector(".tonearm");
+const lp = document.getElementById("lp");
+const audio = document.getElementById("player");
+const clickZone = document.getElementById("click-zone");
 
 let isPlaying = false;
 
-tonearm.addEventListener("click", () => {
+clickZone.addEventListener("click", () => {
   isPlaying = !isPlaying;
+
+  // LP 회전
   lp.style.animationPlayState = isPlaying ? "running" : "paused";
+
+  // 음악 재생/정지
+  if (isPlaying) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
 });
