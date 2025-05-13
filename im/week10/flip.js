2ef9c7e919793b264bc0d30a48594a8cb53d0bbe
s3937979 <s3937979@rmit.edu.vn>
2025-05-13 18:37:41 +1000

    update

--- im/week10/flip.js
+++ im/week10/flip.js
@@ -2,18 +2,18 @@ const card = document.querySelector(".card");
 console.log(card);
 
 // Flip on hover
-// card.addEventListener("mouseenter", flipMe);
-// card.addEventListener("mouseleave", revertBack);
-// function flipMe() {
-//   card.classList.add("flip");
-// }
-// function revertBack() {
-//   card.classList.remove("flip");
-// }
+card.addEventListener("mouseenter", flipMe);
+card.addEventListener("mouseleave", revertBack);
+function flipMe() {
+  card.classList.add("flip");
+}
+function revertBack() {
+  card.classList.remove("flip");
+}
 
 // Flip on click
-card.addEventListener("click", toggleFlip);
+// card.addEventListener("click", toggleFlip);
 
-function toggleFlip() {
-  card.classList.toggle("flip");
-}
+// function toggleFlip() {
+//   card.classList.toggle("flip");
+// }
