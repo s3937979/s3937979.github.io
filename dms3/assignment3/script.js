ad5760faa14936398df49c2050f483775226123b
s3937979 <s3937979@rmit.edu.vn>
2025-05-19 01:13:10 +1000

    update all

--- dms3/assignment3/script.js
+++ dms3/assignment3/script.js
@@ -1,12 +1,14 @@
 const heading = document.getElementById("heading");
-const dialog = document.getElementById("myDialog");
 const mainImage = document.getElementById("main-image");
+const secondPage = document.getElementById("second-page");
+const dialog = document.getElementById("myDialog");
 
 heading.addEventListener("click", () => {
   document.querySelector(".main-page").style.display = "none";
   document.querySelector(".main-page-stars").style.display = "none";
-
   document.querySelector(".second-page").style.display = "block";
+  document.querySelector(".star-character").style.display = "block";
 
+  const dialog = document.getElementById("myDialog");
   dialog.showModal();
 });
