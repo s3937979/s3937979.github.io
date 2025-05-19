73cee6e9c2a53338d8c4b4d3b5a9e3fff37273f2
s3937979 <s3937979@rmit.edu.vn>
2025-05-19 12:12:24 +1000

    update all

--- dms3/assignment3/script.js
+++ dms3/assignment3/script.js
@@ -7,8 +7,23 @@ heading.addEventListener("click", () => {
   document.querySelector(".main-page").style.display = "none";
   document.querySelector(".main-page-stars").style.display = "none";
   document.querySelector(".second-page").style.display = "block";
-  document.querySelector(".star-character").style.display = "block";
 
-  const dialog = document.getElementById("myDialog");
-  dialog.showModal();
+  // 별 캐릭터 먼저 보여주기 (예: 나타났다 사라지게 만들 수도 있음)
+  const star = document.querySelector(".star-character");
+  star.style.display = "block";
+
+  // 💡 dialog는 2초 뒤에 열기
+  setTimeout(() => {
+    dialog.showModal();
+  }, 4000); // 2000ms = 2초
+});
+
+const sendButton = document.querySelector("send-button");
+const thirdPage = document.querySelector("third-page");
+
+sendButton.addEventListener("click", () => {
+  // 두 번째 페이지 숨기고
+  secondPage.style.display = "none";
+  // 세 번째 페이지 보여주기
+  thirdPage.style.display = "flex";
 });
