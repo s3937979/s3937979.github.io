615318a0ecffa2a2dd3cb3ba1ae8acdc37b571e9
s3937979 <s3937979@rmit.edu.vn>
2026-05-18 00:23:11 +0700

    Revert "Make dms5 link use absolute assets"
    This reverts commit d9e95c52d1dfb65499cf208173b95736acdb838a.

--- dms5/vite.config.ts
+++ dms5/vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/dms5/',
+  base: './',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
